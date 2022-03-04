aa1b5dc6767a1e135eefe54ea948989939a67573
Christopher Chantres <chantrex@hotmail.com>
2022-03-03 20:47:21 -0500

    Inventory modules configured corectly

---
+++ controllers/inventory.js
@@ -0,0 +1,16 @@
+let Inventory = require('../models/inventory');
+
+exports.list = function (req, res, next){
+    Inventory.find((err, inventoryList) => {
+        console.log(inventoryList);
+        if(err)
+        {
+            return console.error(err);
+        }
+        else{
+            console.log(inventoryList);
+        }
+    });
+
+   // res.render('index', { title: 'About' });
+}
--- routes/inventory.js
+++ routes/inventory.js
@@ -2,25 +2,11 @@ let express = require('express');
 let router = express.Router();
 
 let mongoose = require('mongoose');
-const inventory = require('../models/inventory');
 
-let Inventory = require('../models/inventory');
+let inventoryController = require ('../controllers/inventory')
 
 
 /* GET Inventory  page. */
-router.get('/list', function (req, res, next){
-    Inventory.find((err, inventoryList) => {
-        console.log(inventoryList);
-        if(err)
-        {
-            return console.error(err);
-        }
-        else{
-            console.log(inventoryList);
-        }
-    });
-
-   // res.render('index', { title: 'About' });
-});
+router.get('/list', inventoryController.list);
 
 module.exports = router;
